0e5d4ec7290f05460dbe156e7f15ca1860030ca3
Nigel Kibodeaux <nigelmail@gmail.com>
2025-04-20 13:12:26 -0800

    make it run on the pi

--- fidelityrewards.com/scraper.mjs
+++ fidelityrewards.com/scraper.mjs
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises'
-import { chromium } from 'playwright-chromium'
+import { chromium } from 'playwright-core'
 
 /**
  * Downloads the CSV file from the Fidelity Rewards website using a headless browser.
@@ -13,47 +13,61 @@ export async function downloadCsvWithHeadlessBrowser() {
 
     // Launch browser
     console.log('Launching browser')
-    const context = await chromium.launchPersistentContext('./user_data', { headless: true })
+    const context = await chromium.launchPersistentContext('./user_data', {
+        headless: true,
+        executablePath: process.env.CHROMIUM_PATH,
+    })
     const pages = context.pages()
     const page = pages[0]
 
-    // Log in
-    console.log('Navigating to login page')
-    await page.goto('https://login.fidelityrewards.com')
-    console.log('Page loaded')
+    try {
+        // Log in
+        console.log('Navigating to login page')
+        await page.goto('https://login.fidelityrewards.com')
+        console.log('Page loaded')
 
-    // Wait for the inputs or a login link
-    await Promise.race([page.locator('a[href*="login.do"]').waitFor(), page.locator('input[name=Username]').waitFor()])
+        // Wait for the inputs or a login link
+        await Promise.race([
+            page.locator('a[href*="login.do"]').waitFor(),
+            page.locator('input[name=Username]').waitFor(),
+        ])
 
-    // If there is a login link, click it and wait for the page to load
-    if (await page.$('a[href*="login.do"]')) {
-        console.log('Login link found. Clicking...')
-        await page.click('a[href*="login.do"]')
-        console.log('Waiting for new page to load')
-        await page.waitForLoadState('load')
+        // If there is a login link, click it and wait for the page to load
+        if (await page.$('a[href*="login.do"]')) {
+            console.log('Login link found. Clicking...')
+            await page.click('a[href*="login.do"]')
+            console.log('Waiting for new page to load')
+            await page.waitForLoadState('load')
+        }
+
+        // Fill in login info
+        console.log('Filling in auth info')
+        await page.fill('input[name=Username]', process.env.FIDELITY_USERNAME)
+        await page.fill('input[name=Password]', process.env.FIDELITY_PASSWORD)
+        await page.click('button[type=submit]')
+        console.log('Submitted login')
+
+        // Download CSV
+        console.log('Waiting for download button')
+        await page.click('#transaction-download-button')
+        // await page.waitForSelector('#transactionsDownloadOptions', { state: 'visible' })
+        await page.click('#transactionsDownloadOptionsDownload')
+        console.log('Download button clicked')
+
+        // Wait for download to complete
+        console.log('Waiting for download to complete')
+        const download = await page.waitForEvent('download')
+        await download.saveAs('./transactions.csv')
+
+        return fs.readFile('./transactions.csv', 'utf-8')
+    } catch (error) {
+        console.error('Error during download:', error)
+
+        // Take a screenshot for debugging
+        await page.screenshot({ path: 'screenshot.png' })
+        console.error('Screenshot saved as screenshot.png')
+    } finally {
+        // Close browser
+        await context.close()
     }
-
-    // Fill in login info
-    console.log('Filling in auth info')
-    await page.fill('input[name=Username]', process.env.FIDELITY_USERNAME)
-    await page.fill('input[name=Password]', process.env.FIDELITY_PASSWORD)
-    await page.click('button[type=submit]')
-    console.log('Submitted login')
-
-    // Download CSV
-    console.log('Waiting for download button')
-    await page.click('#transaction-download-button')
-    // await page.waitForSelector('#transactionsDownloadOptions', { state: 'visible' })
-    await page.click('#transactionsDownloadOptionsDownload')
-    console.log('Download button clicked')
-
-    // Wait for download to complete
-    console.log('Waiting for download to complete')
-    const download = await page.waitForEvent('download')
-    await download.saveAs('./transactions.csv')
-
-    // Close browser
-    await context.close()
-
-    return fs.readFile('./transactions.csv', 'utf-8')
 }
